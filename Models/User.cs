a31382cc3c0c8f040ddc5fb644bce6bfd056d2ad
Eligh John <eligh.john@deltacapita.com>
2022-08-09 14:30:54 +0100

    Form interface for signing up user

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signup_example.Models
+{
+    public class User
+    {
+
+        [Required(ErrorMessage = "Please enter email...")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter password...")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm password...")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
